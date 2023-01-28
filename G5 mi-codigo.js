84a98e397ad92e988331df8ea97bb35d473c6026
JorviGames <119646184+JorviGames@users.noreply.github.com>
2023-01-27 20:01:24 -0600

    Update G5 mi-codigo.js

--- G5 mi-codigo.js
+++ G5 mi-codigo.js
@@ -1,5 +1,7 @@
-alert("Hola mundo precioso siuuuuuu");
+alert("Hola mundo precioso siuuuuuu __ Nueva version messi campeon");
 
 commit changes
 
 // hola soy timido 
+
+console.log("Hubieron cambios en el archivo");
